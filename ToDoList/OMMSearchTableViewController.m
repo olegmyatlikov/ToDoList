66f1425a67115e8b13b97b6a1a0f2aa5d250c182
Oleg Myatlikov <olegmyatlikov@gmail.com>
2017-06-24 22:48:40 +0300

    [JIRA-1] Work with Search tab

--- ToDoList/OMMSearchTableViewController.m
+++ ToDoList/OMMSearchTableViewController.m
@@ -17,7 +17,6 @@
 
 @property (strong, nonatomic) OMMTaskService *taskService;
 @property (nonatomic, strong) NSArray *resultTaskArray;
-@property (nonatomic, strong) NSArray *tasksArray;
 @property (nonatomic, strong) UISearchController *searchController;
 
 @property (nonatomic, strong) NSPredicate *activeTasksPredicate;
@@ -27,21 +26,27 @@
 
 @implementation OMMSearchTableViewController
 
+static NSString * const OMMsearchTaskIsOpen = @"closed = 0";
+static NSString * const OMMsearchTaskIsClosed = @"closed = 1";
+static NSString * const OMMsearchActiveTasks = @"Active tasks";
+static NSString * const OMMsearchComplitedTasks = @"Completed";
+static NSString * const OMMsearchClearText = @"";
+static NSString * const OMMsearchNoResultText = @"No Result";;
+
 - (void)viewDidLoad {
     [super viewDidLoad];
     
     self.taskService = [OMMTaskService sharedInstance];
-    self.activeTasksPredicate = [NSPredicate predicateWithFormat:@"closed = 0"];
-    self.closedTasksPredicate = [NSPredicate predicateWithFormat:@"closed = 1"];
+    self.activeTasksPredicate = [NSPredicate predicateWithFormat:OMMsearchTaskIsOpen];
+    self.closedTasksPredicate = [NSPredicate predicateWithFormat:OMMsearchTaskIsClosed];
     
-    self.tasksArray = [self.taskService.allTasksArray filteredArrayUsingPredicate:self.activeTasksPredicate];
-    self.resultTaskArray = self.tasksArray;
+    self.resultTaskArray = [self filterArrayUsingSelectScopeButton:self.taskService.allTasksArray];
     
     self.searchController = [[UISearchController alloc] initWithSearchResultsController:nil];
     self.searchController.searchResultsUpdater = self;
     self.searchController.searchBar.delegate = self;
     self.searchController.dimsBackgroundDuringPresentation = NO;
-    self.searchController.searchBar.scopeButtonTitles = @[@"Active tasks", @"Completed"];
+    self.searchController.searchBar.scopeButtonTitles = @[OMMsearchActiveTasks, OMMsearchComplitedTasks];
     
     self.tableView.tableHeaderView = self.searchController.searchBar;
     self.definesPresentationContext = YES; // hide searchController if present another viewController and do visible if go back
@@ -56,44 +61,20 @@
 }
 
 - (void)triggerTaskWasCreatedOrEdited:(NSNotification *)notification {
-    self.tasksArray = [self.taskService.allTasksArray filteredArrayUsingPredicate:self.activeTasksPredicate];
-    self.resultTaskArray = self.tasksArray;
+    self.resultTaskArray = [self filterArrayUsingSelectScopeButton:self.taskService.allTasksArray];
+    self.searchController.searchBar.text = OMMsearchClearText;
     [self.tableView reloadData];
 }
 
-#pragma mark - Table view data source
-
-- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
-{
-    if (self.resultTaskArray.count > 0) {
-        self.tableView.backgroundView = nil;
-        
-        return 1;
+- (NSArray *)filterArrayUsingSelectScopeButton:(NSArray *)array {
+    NSArray *resultArray = [[NSArray alloc] init];
+    if (self.searchController.searchBar.selectedScopeButtonIndex == 0) {
+        resultArray = [array filteredArrayUsingPredicate:self.activeTasksPredicate];
     } else {
-        UILabel *noResultLabel = [[UILabel alloc] initWithFrame:CGRectMake(0, 0, self.view.bounds.size.width, self.view.bounds.size.height)];
-        noResultLabel.text = @"No Result";
-        [noResultLabel setFont:[UIFont systemFontOfSize:24 weight:normal]];
-        noResultLabel.textColor = [UIColor lightGrayColor];
-        noResultLabel.textAlignment = NSTextAlignmentCenter;
-        self.tableView.backgroundView = noResultLabel;
-        
-        return 0;
+        resultArray = [array filteredArrayUsingPredicate:self.closedTasksPredicate];
     }
+    return resultArray;
 }
-
-// select scope another button
-
-- (void)searchBar:(UISearchBar *)searchBar selectedScopeButtonIndexDidChange:(NSInteger)selectedScope {
-    if (selectedScope == 0) {
-        self.tasksArray = [self.taskService.allTasksArray filteredArrayUsingPredicate:self.activeTasksPredicate];
-    } else {
-        self.tasksArray = [self.taskService.allTasksArray filteredArrayUsingPredicate:self.closedTasksPredicate];
-    }
-    self.resultTaskArray = self.tasksArray;
-    [self.tableView reloadData];
-    self.searchController.searchBar.text = @"";
-}
-
 
 //
 //- (UIView *)tableView:(UITableView *)tableView viewForHeaderInSection:(NSInteger)section {
@@ -115,6 +96,9 @@
 //- (NSString *)tableView:(UITableView *)tableView titleForHeaderInSection:(NSInteger)section {
 //    return @" ";
 //}
+
+
+#pragma mark - Table view data source
 
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
     return [self.resultTaskArray count];
@@ -139,6 +123,13 @@
     return 68.f;
 }
 
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
+    OMMTask *task = [self.resultTaskArray objectAtIndex:indexPath.row];
+    OMMTaskDetailTableVC *taskDetails = [self.storyboard instantiateViewControllerWithIdentifier:@"OMMTaskDetailVCIndentifair"];
+    taskDetails.task = task;
+    [self.navigationController pushViewController:taskDetails animated:YES];
+}
+
 
 #pragma mark - search and update result
 
@@ -147,22 +138,48 @@
 }
 
 - (void)searchBar:(UISearchBar *)searchBar textDidChange:(NSString *)searchText {
-    self.resultTaskArray = self.tasksArray;
+    self.resultTaskArray = [self filterArrayUsingSelectScopeButton:self.taskService.allTasksArray];
+    
     NSPredicate *predicate = [NSPredicate predicateWithFormat:@"name contains[cd] %@", searchText];
     if (searchText.length != 0) {
-        self.resultTaskArray = [self.tasksArray filteredArrayUsingPredicate:predicate];
+        self.resultTaskArray = [self.resultTaskArray filteredArrayUsingPredicate:predicate];
     }
 }
 
-- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
-    OMMTask *task = [self.resultTaskArray objectAtIndex:indexPath.row];
-    OMMTaskDetailTableVC *taskDetails = [self.storyboard instantiateViewControllerWithIdentifier:@"OMMTaskDetailVCIndentifair"];
-    taskDetails.task = task;
-    [self.navigationController pushViewController:taskDetails animated:YES];
+// "No result" on dispaly if resultArray is empty
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
+{
+    if (self.resultTaskArray.count > 0) {
+        self.tableView.backgroundView = nil;
+        
+        return 1;
+    } else {
+        UILabel *noResultLabel = [[UILabel alloc] initWithFrame:CGRectMake(0, 0, self.view.bounds.size.width, self.view.bounds.size.height)];
+        noResultLabel.text = OMMsearchNoResultText;
+        [noResultLabel setFont:[UIFont systemFontOfSize:24 weight:normal]];
+        noResultLabel.textColor = [UIColor lightGrayColor];
+        noResultLabel.textAlignment = NSTextAlignmentCenter;
+        self.tableView.backgroundView = noResultLabel;
+        
+        return 0;
+    }
+}
+
+// select scope another button
+
+- (void)searchBar:(UISearchBar *)searchBar selectedScopeButtonIndexDidChange:(NSInteger)selectedScope {
+    if (selectedScope == 0) {
+        self.resultTaskArray = [self.taskService.allTasksArray filteredArrayUsingPredicate:self.activeTasksPredicate];
+    } else {
+        self.resultTaskArray = [self.taskService.allTasksArray filteredArrayUsingPredicate:self.closedTasksPredicate];
+    }
+    [self.tableView reloadData];
+    self.searchController.searchBar.text = OMMsearchClearText;
 }
 
 - (void)searchBarCancelButtonClicked:(UISearchBar *)searchBar {
-    self.resultTaskArray = [self.taskService.allTasksArray mutableCopy];
+    self.resultTaskArray = [self filterArrayUsingSelectScopeButton:self.taskService.allTasksArray];
 }
 
 
